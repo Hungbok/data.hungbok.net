591a17d9b7783097190255c895efe92df305c5cc
Hungbok <bjminimn@naver.com>
2024-03-04 04:31:03 +0900

    Update

--- javascript/games-quarter.js
+++ javascript/games-quarter.js
@@ -50,76 +50,51 @@ if (year && season && monthRange) {
             <p>${month}</p>
           `;
           calendarDiv.appendChild(monthDiv);
-
-          // 이전 섹션으로 이동하는 버튼 추가
-          if (lastMonth) {
-            var prevButton = document.createElement('button');
-            prevButton.textContent = lastMonth;
-            prevButton.onclick = function(event) {
-              event.preventDefault();
-              document.getElementById('section-' + lastMonth).scrollIntoView({behavior: "smooth"});
-            };
-            monthDiv.appendChild(prevButton);
-          }
-
-          // 다음 섹션으로 이동하는 버튼 추가
-          if (index < filteredData.length - 1) {
-            var nextMonth = filteredData[index + 1].date.split('-')[1];
-            if (month !== nextMonth) {
-              var nextButton = document.createElement('button');
-              nextButton.textContent = nextMonth;
-              nextButton.onclick = function(event) {
-                event.preventDefault();
-                document.getElementById('section-' + nextMonth).scrollIntoView({behavior: "smooth"});
-              };
-              monthDiv.appendChild(nextButton);
-            }
-          }
       
           lastMonth = month;
         }
         
         // 'url' 필드 값을 이용하여 '/games/[url값].json' 파일 불러오기
         fetch('//data.hungbok.net/data/games/' + item.url + '.json')
-          .then(response => {
-            if (!response.ok) throw new Error('Not Found');
-            return response.json();
-          })
-          .then(gameData => {
-            var div = document.createElement('div');
-            div.className = 'calendar-item';
-            div.innerHTML = `
-              <a href="https://www.hungbok.com/games?q=${item.url}">
-                <div class="calendar-item-background">
-                  <img src="https://data.hungbok.net/image/games/${item.url}/hb_capsule.jpg" onerror="this.onerror=null; this.src='//data.hungbok.net/image/hb/hb_error_horizontal.svg'">
+        .then(response => {
+          if (!response.ok) throw new Error('Not Found');
+          return response.json();
+        })
+        .then(gameData => {
+          var div = document.createElement('div');
+          div.className = 'calendar-item';
+          div.innerHTML = `
+            <a href="https://www.hungbok.com/games?q=${item.url}">
+              <div class="calendar-item-background">
+                <img src="https://data.hungbok.net/image/games/${item.url}/hb_capsule.jpg" onerror="this.onerror=null; this.src='//data.hungbok.net/image/hb/hb_error_horizontal.svg'">
+              </div>
+              <div class="calendar-item-info">
+                <div class="calendar-item-title">
+                  <p>${gameData[languageCode].title}</p>
                 </div>
-                <div class="calendar-item-info">
-                  <div class="calendar-item-title">
-                    <p>${gameData[languageCode].title}</p>
-                  </div>
-                  <div class="calendar-item-date">
-                    <p class="calendar-item-year">${item.date.split('-')[0]}</p>
-                    <p class="calendar-item-month">${item.date.split('-')[1]}</p>
-                    <p class="calendar-item-day">${item.date.split('-')[2]}</p>
-                  </div>
-                  <div class="calendar-item-platform ${item.platform}">
-                    <img class="display" src="https://data.hungbok.net/image/icon/display.svg" onerror="this.src='//data.hungbok.net/image/hb/hb_error.svg';">
-                    <img class="playstation" src="https://data.hungbok.net/image/icon/playstation.svg" onerror="this.src='//data.hungbok.net/image/hb/hb_error.svg';">
-                    <img class="xbox" src="https://data.hungbok.net/image/icon/xbox.svg" onerror="this.src='//data.hungbok.net/image/hb/hb_error.svg';">
-                    <img class="nintendo" src="https://data.hungbok.net/image/icon/nintendo.svg" onerror="this.src='//data.hungbok.net/image/hb/hb_error.svg';">
-                    <img class="phone" src="https://data.hungbok.net/image/icon/phone.svg" onerror="this.src='//data.hungbok.net/image/hb/hb_error.svg';">
-                    <img class="cloud" src="https://data.hungbok.net/image/icon/cloud.svg" onerror="this.src='//data.hungbok.net/image/hb/hb_error.svg';">
-                  </div>
+                <div class="calendar-item-date">
+                  <p class="calendar-item-year">${item.date.split('-')[0]}</p>
+                  <p class="calendar-item-month">${item.date.split('-')[1]}</p>
+                  <p class="calendar-item-day">${item.date.split('-')[2]}</p>
                 </div>
-              </a>
-            `;
-            calendarDiv.appendChild(div);
-          })
-          .catch(error => {
-            console.error('Error:', error);
-            // '/games/[url값].json' 파일이 없는 경우
-            $('main > .top-backgrounds').remove();
-          });
+                <div class="calendar-item-platform ${item.platform}">
+                  <img class="display" src="https://data.hungbok.net/image/icon/display.svg" onerror="this.src='//data.hungbok.net/image/hb/hb_error.svg';">
+                  <img class="playstation" src="https://data.hungbok.net/image/icon/playstation.svg" onerror="this.src='//data.hungbok.net/image/hb/hb_error.svg';">
+                  <img class="xbox" src="https://data.hungbok.net/image/icon/xbox.svg" onerror="this.src='//data.hungbok.net/image/hb/hb_error.svg';">
+                  <img class="nintendo" src="https://data.hungbok.net/image/icon/nintendo.svg" onerror="this.src='//data.hungbok.net/image/hb/hb_error.svg';">
+                  <img class="phone" src="https://data.hungbok.net/image/icon/phone.svg" onerror="this.src='//data.hungbok.net/image/hb/hb_error.svg';">
+                  <img class="cloud" src="https://data.hungbok.net/image/icon/cloud.svg" onerror="this.src='//data.hungbok.net/image/hb/hb_error.svg';">
+                </div>
+              </div>
+            </a>
+          `;
+          calendarDiv.appendChild(div);
+        })
+        .catch(error => {
+          console.error('Error:', error);
+          // '/games/[url값].json' 파일이 없는 경우
+          $('main > .top-backgrounds').remove();
+        });
       });
     })
     .catch(error => {
