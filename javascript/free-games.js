7964cf9e15332f5cac5b908d34559b55af80d401
Hungbok <bjminimn@naver.com>
2024-03-06 16:13:03 +0900

    Update

--- javascript/free-games.js
+++ javascript/free-games.js
@@ -1,71 +1,23 @@
 let data = [];
 let filteredData = [];
 let start = 0;
-let limit = 10;
+let limit = 16;
 
 // JSON 파일 불러오기
 fetch('//data.hungbok.net/data/free-games.json')
 .then(response => response.json())
 .then(json => {
-    data = json.map(item => {
-        const now = new Date();
-        const startTime = new Date(item.start.replace(/-/g, '/'));
-        const endTime = new Date(item.end.replace(/-/g, '/'));
-        if (now >= startTime) {
-            item.started = true;
-        }
-        if (now >= endTime) {
-            item.ended = true;
-        }
-        return item;
-    });
+    data = json;
     filteredData = [...data];
     loadMoreData();
 });
 
 // 필터링 기능
-function filterData(type) {
+function filterData(esd, key) {
     start = 0;
-    const filterStarted = document.getElementById('filter-started').checked;
-    const filterEnded = document.getElementById('filter-ended').checked;
-    if (type === 'all' || type === 'check') {
-        filteredData = [...data];
-    } else {
-        filteredData = data.filter(item => item.type === type);
-    }
-    if (filterStarted) {
-        filteredData = filteredData.filter(item => item.started);
-    }
-    if (filterEnded) {
-        filteredData = filteredData.filter(item => item.ended);
-    }
-    document.getElementById('dataContainer').innerHTML = '';
+    filteredData = data.filter(item => item.esd === esd && item.key === key);
+    document.querySelector('.container').innerHTML = '';
     loadMoreData();
-}
-
-// 타이머 생성 함수
-function createTimer(timerContainer, endTime) {
-    const targetTime = new Date(endTime.replace(/-/g, '/'));
-    const timerInterval = setInterval(function() {
-        const now = new Date();
-        const difference = targetTime - now;
-        let formattedTime;
-        if (difference > 0) {
-            const seconds = Math.floor(difference / 1000) % 60;
-            const minutes = Math.floor(difference / 1000 / 60) % 60;
-            const hours = Math.floor(difference / 1000 / 60 / 60) % 24;
-            const days = Math.floor(difference / 1000 / 60 / 60 / 24);
-            if (days > 0) {
-                formattedTime = `${days}:${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-            } else {
-                formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-            }
-        } else {
-            clearInterval(timerInterval);
-            formattedTime = "00:00:00";
-        }
-        timerContainer.textContent = formattedTime;
-    }, 1000);
 }
 
 // 무한 스크롤 기능
@@ -88,16 +40,25 @@ function loadMoreData() {
             <a class="item-link" href="${item.link}" target="_blank"></a>
             <img class="item-background" src="${item.image}">
         `;
-        document.getElementById('dataContainer').appendChild(div);
-
-        createTimer(div.querySelector('.start'), item.start);
-        createTimer(div.querySelector('.end'), item.end);
+        document.querySelector('.container').appendChild(div);
+        loadScript('//data.hungbok.net/javascript/free-games-timer.js', function() {
+            console.log('Script loaded for item:', item.title);
+        });
     });
 }
 
 // 스크롤 이벤트
 window.addEventListener('scroll', () => {
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 500) {
         loadMoreData();
     }
 });
+
+// 스크립트 동적 로딩 함수
+function loadScript(url, callback){
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+    script.onload = callback;
+    document.head.appendChild(script);
+}
